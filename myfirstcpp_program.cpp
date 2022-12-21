7d39e1941da4119f502aec1fc6a030676e36ce27
Carlos Murilo Romero Rocha <114645116+Cmurilochem@users.noreply.github.com>
2022-12-21 20:09:43 +0100

    Update myfirstcpp_program.cpp

--- myfirstcpp_program.cpp
+++ myfirstcpp_program.cpp
@@ -3,7 +3,7 @@
 #include <string> // when dealing with strings 
 #include "outside_func.h" // file with external functions 
 
-using namespace std; // OK! Testing git
+using namespace std; // OK! Testing git - testing branches
 
 //
 /* Intersting features of C++: structures, references, pointers */
